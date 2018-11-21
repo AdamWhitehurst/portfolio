237173ef30f1984b3d9b7e264ec7402f9d332830
Adam <Adam.Whitehurst@live.com>
2018-11-20 18:24:12 -0800

    Initial implement react-slick-carasoul

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Sectional from './components/sectional';
+import { SimpleSlider } from './components/simple_slider';
 
 class App extends Component {
+  renderCarasoulItems() {
+    let items = [];
+    for (let i = 0; i < 10; i++) 
+    items.push (
+    <div key={i} className="carasoul-item">
+      <p>HI</p>
+    </div>
+    );
+    return items;
+  }
   renderSectionals () {
     let sectionals = [];
     for (let i = 0; i < 10; i++) 
-    sectionals.push (<Sectional className="sectional"/>);
+    sectionals.push (<Sectional key={i} className="sectional"/>);
     return sectionals;
   }
   render() {
@@ -15,8 +26,10 @@ class App extends Component {
         <header className="header">
           <p>I AM A WIP AND I AM UGLY</p>
         </header>
-        <div className="carasoul">
-          <p>ONE DAY I WILL SPIN FOR I AM THE CARASOUL</p>
+        <div className="carasoul-container">
+          <SimpleSlider className="carasoul">
+          {this.renderCarasoulItems()}
+          </SimpleSlider>
         </div>
         <div className="content">
         {this.renderSectionals()}
--- src/components/sectional.js
+++ src/components/sectional.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Sectional extends Component {
   render() {
     return (
-      <div  className={this.props.className}>
+      <div className={this.props.className}>
       <p>I AM A SECTIONALLLLLLLLLLLL</p>
       </div>
       );
