dee75188f756f93dfac400c518de1c9a8ec3bec6
Adam <Adam.Whitehurst@live.com>
2018-11-21 13:14:35 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="main">
+      <div className="main"> 
         <header className="header">
           <h2>Adam Whitehur.st</h2>
         </header>
