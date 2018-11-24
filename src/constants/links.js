5d80b56b019fecf236968e58a3fcb98999fee2ba
Adam <Adam.Whitehurst@live.com>
2018-11-23 16:15:05 -0800

    Add links

---
+++ src/constants/links.js
@@ -0,0 +1,18 @@
+export const Links = {
+  "Github": {
+      url: "https://github.com/AdamWhitehurst",
+      icon: "github-alt"
+  },
+  "LinkedIn": {
+      url: "https://www.linkedin.com/in/adam-whitehurst-970504b0/",
+      icon: "linkedin-in"
+  },
+  "Facebook": {
+      url: "https://www.facebook.com/WittyHorse",
+      icon: "facebook-f"
+  },
+  "Steam Workshop": {
+      url: "https://steamcommunity.com/profiles/76561198121157048/myworkshopfiles/",
+      icon: "steam-symbol"
+  }
+};
