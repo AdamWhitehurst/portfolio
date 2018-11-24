cda1d7cdfa33e78403e0497bef9037626b094fbe
Adam <Adam.Whitehurst@live.com>
2018-11-23 16:14:51 -0800

    Refactor Carasoul logic

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import { Header } from './components/header';
 import { Carasoul } from './components/carasoul';
 
 class App extends Component {
-  renderCarasoulItems() {
-    let items = [];
-    for (let i = 0; i < 10; i++) 
-    items.push (
-
-    );
-    return items;
-  }
 
   renderSectionals () {
     let sectionals = [];
     for (let i = 0; i < 10; i++) 
     sectionals.push (
-
     );
     return sectionals;
   }
@@ -26,31 +17,11 @@ class App extends Component {
     return (
       <Container>
         <Header/>
-        <Carasoul/>
+        <Carasoul flex={2}>
+        </Carasoul>
         <Content>
-          <p>2</p>
+
         </Content>
-        <ContentSecondary>
-          <p>3</p>
-        </ContentSecondary>
-        <Content>
-          <p>4</p>
-        </Content>
-        <ContentSecondary>
-          <p>5</p>
-        </ContentSecondary>
-        <Content>
-          <p>6</p>
-        </Content>
-        <ContentSecondary>
-          <p>7</p>
-        </ContentSecondary>
-        <Content>
-          <p>8</p>
-        </Content>
-        <ContentSecondary>
-          <p>9</p>
-        </ContentSecondary>
       </Container>
     );
   }
