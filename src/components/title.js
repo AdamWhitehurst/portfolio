e1fbf7aed6d07f7b81290cda5ec865367c994ca7
Adam <Adam.Whitehurst@live.com>
2018-11-27 10:17:25 -0800

    Add contact to title, Styling

--- src/components/title.js
+++ src/components/title.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import injectSheet from 'react-jss';
-import { MainTitle } from '../constants/texts';
+import { MainTitle, ContactText } from '../constants/texts';
 
-
-const style = (Theme) => ({
-  title: {
+const style = (theme) => ({
+  container: {
+    height: theme.titleFontSize,
+    display: 'flex',
+    flexDirection: 'row-reverse',
+  },
+  mainTitle: {
     display: 'flex',
     alignItems: 'baseline',
     justifyContent: 'flex-start',
@@ -12,26 +16,62 @@ const style = (Theme) => ({
     verticalAlign: 'baseline',
     padding: 0,
     margin: 0,
-    color: Theme.textLight,
+    color: theme.textLight,
     fontWeight: '100',
-    fontFamily: Theme.titleFontFamily,
-    textShadow: Theme.outTextShadow,
-    fontSize: Theme.titleFontSize,
+    fontFamily: theme.titleFontFamily,
+    textShadow: theme.outTextShadow,
+    fontSize: theme.titleFontSize,
     lineHeight: '85%',
     position: 'fixed',
     left: 0,
-    paddingRight: '0.15em',
+    paddingRight: '0.25em',
     marginLeft: '0.15em',
-    borderBottom: `0.2rem solid ${Theme.textLight}`,
+    borderBottom: `0.2rem solid ${theme.textLight}`,
     borderRadius: '25%',
     borderBottomLeftRadius: '8%',
-    boxShadow: Theme.outBorderShadow,
+    boxShadow: theme.outBorderShadow,
     zIndex: 99,
-  }
+  },
+  contactContainer: {
+    position: 'fixed',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '0.75rem',
+    paddingTop: 0,
+    color: theme.textLight,
+    fontWeight: '100',
+    zIndex: 99,
+    borderLeft: `0.06em solid ${theme.colorPrimaryLight}`,
+  },
+    contactTitle: {
+    display: 'flex',
+    alignSelf: 'flex-start',
+    alignItems: 'baseline',
+    textShadow: theme.outTextShadow,
+    justifyContent: 'flex-end',
+    alignContent: 'flex-end',
+    verticalAlign: 'baseline',
+    fontWeight: '100',
+    margin: 0,
+  },
+  contactText: {
+    fontFamily: theme.contentFontFamily,
+    fontWeight: '100',
+    textShadow: theme.outTextShadow,
+    margin: 0,
+  },
 });
 
 const UnstyledTitle = ({classes, children}) => (
-  <h1 className={classes.title}>{MainTitle}</h1>
+  <div className={classes.container}>
+    <h1 className={classes.mainTitle}>{MainTitle}</h1>
+    <div className={classes.contactContainer}>
+      <h4 className={classes.contactTitle}>{ContactText.Title}</h4>
+      <h6 className={classes.contactText}>{ContactText.Address}</h6>
+      <h6 className={classes.contactText}>{ContactText.Email}</h6>
+      <h6 className={classes.contactText}>{ContactText.Phone}</h6>
+    </div>
+  </div>
 );
 
 export const Title = injectSheet(style)(UnstyledTitle);
